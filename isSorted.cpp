a79ed466f67b90d737e114deb584ca4ca18482e4
Shivani Kumari <100162080+63Shivani@users.noreply.github.com>
2022-11-16 14:56:07 +0530

    Create isSorted.cpp

---
+++ isSorted.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+bool isSorted(int *arr,int size){
+	if(size==0 || size==1){
+		return true;
+	}
+	if(arr[0]>arr[1])
+	return false;
+	else{
+		bool remainingPart=isSorted(arr+1,size-1);
+		return remainingPart;
+	}
+}
+int main(){
+	int arr[5]={2,4,5,7,9};
+	int size=5;
+	bool ans=isSorted(arr,size);
+	if(ans){
+		cout<<"Array is Sorted"<<endl;
+	}
+	else{
+		cout<<"Not Sorted"<<endl;
+	}
+	return 0;
+}
